0470c92453d1746307e0bc4ed45728530e0e638d
ภูวดล สารีกิจ <s6002041520221@Macmini-213-29.local>
2019-03-14 16:38:13 +0700

    create App component

---
+++ components/App.js
@@ -0,0 +1,17 @@
+// import Library
+import React from 'react';
+import { View, Text } from 'react-native';
+
+// write components
+class App extends React.Component {
+    render(){
+        return (
+            //component about react native
+            <View>
+                <Text>Hello World</Text>
+            </View>
+        );
+    }
+}
+// export
+export default App;
