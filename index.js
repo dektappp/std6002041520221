8b51fce2246f91a478377da177461a8ebe4f6293
ภูวดล สารีกิจ <s6002041520221@Macmini-213-29.local>
2019-04-11 17:02:54 +0700

    NameEmail

--- index.js
+++ index.js
@@ -3,8 +3,10 @@
  */
 
 import {AppRegistry} from 'react-native';
+import {createStackNaviga}
 //import App from './App';
 import {name as appName} from './app.json';
 import App from './components/App';
+import About from './components/About';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => About);
